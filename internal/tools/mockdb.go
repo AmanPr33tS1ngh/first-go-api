3ddc0f5f862374c0ae0ef66f093c8cac3cae8231
AmanPr33tS1ngh <amanpr333tsingh01@gmail.com>
2024-02-23 23:00:24 +0530

    created api in go

---
+++ internal/tools/mockdb.go
@@ -0,0 +1,43 @@
+package tools
+
+import ("time")
+
+type mockDB struct{}
+
+var mockLoginDetails map[string]LoginDetails{
+	"alex": {
+		AuthToken: "123ABC"
+		Username: "Alex"
+	}
+}
+
+var mockCoinDetails map[string]CoinDetails{
+	"alex": {
+		Coins: 12
+		Username: "Alex"
+	}
+}
+
+func (d *mockDB) GetUserLoginDetails(username string) *LoginDetails{
+	time.Sleep(time.Second * 1)
+	var clientData = LoginDetails{}
+	clientData, ok := mockLoginDetails[username]
+	if !ok{
+		return nil
+	}
+	return &clientData
+}
+
+func (d *mockDB) GetUserCoins(username string) *CoinDetails{
+	time.Sleep(time.Second * 1)
+	var clientData = CoinDetails{}
+	clientData, ok := mockCoinDetails[username]
+	if !ok{
+		return nil
+	}
+	return &clientData
+}
+
+func (d *mockDB) SetupDatabase() error{
+	return nil
+}
